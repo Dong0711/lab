78afbc3abfe16f5bf352b248d1fbd32a723ca068
Đông Vũ <107346302+Dong0711@users.noreply.github.com>
2023-03-16 21:58:13 +0700

    Add files via upload

---
+++ ThucHanhBuoi1/ThucHanhBuoi1/MainForm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ThucHanhBuoi1
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var tenDangNhap = txtTen.Text;
+            MessageBox.Show("Xin chào bạn " + tenDangNhap + ". Rất vui được gặp bạn", "thông điệp chào mừng");
+        }
+
+        private void txtTen_TextChanged(object sender, EventArgs e)
+        {
+            txtSaoChep.Text = txtTen.Text;
+        }
+
+        private void bnt_SaoChep_Click(object sender, EventArgs e)
+        {
+            txtSaoChep.Text = txtTen.Text;
+        }
+    }
+}
